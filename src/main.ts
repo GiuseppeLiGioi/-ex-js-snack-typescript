42cf9d651dcc04843552dc093e5a594aa8712eaa
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-06-11 16:52:55 +0200

    added. snack 1 + bonus 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log("ciao")
+/*
+🎯 BONUS
+Se è null: stampa “Il dato è vuoto”
+Se è un array: stampa la sua lunghezza
+Se è una Promise: attendi che si risolva e stampa il valore del resolve.
+*/
+
+let valore : unknown = null;
+if(typeof valore === "string"){
+  console.log(valore.toUpperCase())
+}else if(typeof valore === "number"){
+  console.log(valore * 2)
+} else if(typeof valore === "boolean"){
+  console.log(valore)
+} else if(valore === null){
+  console.log("Il dato è vuoto")
+}else if(Array.isArray(valore)){
+  console.log("lunghezza", valore.length, "è un array")
+} else if(valore instanceof Promise){
+ valore.then((msg) => console.log(msg))
+}else{
+  console.log("Tipo non supportato")
+}
